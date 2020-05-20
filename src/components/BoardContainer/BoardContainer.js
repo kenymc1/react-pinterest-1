7edfa9d4eac0f8ce5fa6b61557866da3b66ebfdb
Zoe Ames <zoeames@gmail.com>
2020-05-19 19:47:45 -0500

    single board displays

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './BoardContainer.scss';
 
 import boardsData from '../../helpers/data/boardsData';
@@ -7,6 +9,10 @@ import authData from '../../helpers/data/authData';
 import Board from '../Board/Board';
 
 class BoardContainer extends React.Component {
+  static propTypes = {
+    setSingleBoard: PropTypes.func.isRequired,
+  }
+
   state = {
     boards: [],
   }
@@ -19,7 +25,9 @@ class BoardContainer extends React.Component {
 
   render() {
     const { boards } = this.state;
-    const makeBoards = boards.map((board) => <Board key={board.id} board={board}/>);
+    const { setSingleBoard } = this.props;
+
+    const makeBoards = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard}/>);
 
     return (
       <div className="BoardContainer">
